3ed353069e9f88b34de3232062d462d4e4ed15cb
Minh Truong <ldcc.truongtran@lotte.vn>
2024-05-12 11:43:30 +0700

    Add project files.

---
+++ MyWebAPI/Models/Empleado.cs
@@ -0,0 +1,11 @@
+﻿namespace MyWebAPI.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? Correo {  get; set; }
+        public double Sueldo { get; set; }
+        public string? FechaContrato { get; set; }
+    }
+}
